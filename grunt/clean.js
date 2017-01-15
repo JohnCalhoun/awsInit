ae8b594aae9697d812559934e0e15f1db463bcc8
John M Calhoun <johnmcalhoun123@gmail.com>
2017-01-14 21:35:11 -0600

    index function outlined

---
+++ grunt/clean.js
@@ -0,0 +1,7 @@
+module.exports={
+    options:{
+        "no-write":false
+    },
+    all:["*/node_modules","node_modules"],
+}
+
---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+//parse command line
+    //cloudformation
+    //lambda
+    //customresource
+
+//cp files to source
+console.log('hello')
